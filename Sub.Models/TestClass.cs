8a36806169c4b2fa050fc3a50736efb42aec02e5
Ali <avohra@elevatesaas.com>
2021-07-09 16:47:05 +0530

    added send grid

--- Sub.Models/TestClass.cs
+++ Sub.Models/TestClass.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
 
 namespace Sub.Models
 {
@@ -13,5 +16,18 @@ namespace Sub.Models
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
         public string AnotherName { get; set; }
+
+        public async Task UseSendGridAsync()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("test@example.com", "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
     }
 }
