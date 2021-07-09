d22d7fcd7e77b7ec9e8cb28fc34584d9bf4ff27d
Ali <avohra@elevatesaas.com>
2021-07-09 13:51:02 +0530

    new field added

--- Sub.Models/TestClass.cs
+++ Sub.Models/TestClass.cs
@@ -7,5 +7,6 @@ namespace Sub.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
     }
 }
